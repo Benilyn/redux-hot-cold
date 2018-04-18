cda28bad0396b6d1d5a45df1f4d3cc1c79fbafca
Benilyn <benilyn.gonzales@gmail.com>
2018-04-18 11:04:55 -0700

    add code for action.makeGuess and update action.auralUpdate to reflect state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,9 +9,41 @@ const initialState = {
 
 export const reduxHotCold = (state=initialState, action) => {
 	if (action.type === actions.MAKE_GUESS) {
-		return Object.assign({}, state, {
-			guess: action.guess
-		});
+		let guess = parseInt(action.guess);
+		const difference = Math.abs(guess - initialState.correctAnswer);
+
+    	if (isNaN(guess)) {
+    		return Object.assign({}, state, {
+    			feedback: 'Please enter a valid number'
+    		});
+      	}
+
+      	if (difference >= 50) {
+      		return Object.assign({}, state, {
+    			feedback: 'You\'re Ice Cold...',
+    			guesses: [...state.guesses, action.guess]
+    		});
+	    } else if (difference >= 30) {
+	    	return Object.assign({}, state, {
+    			feedback: 'You\'re Cold...',
+    			guesses: [...state.guesses, action.guess]
+    		});
+	    } else if (difference >= 10) {
+	    	return Object.assign({}, state, {
+    			feedback: 'You\'re Warm.',
+    			guesses: [...state.guesses, action.guess]
+    		});
+	    } else if (difference >= 1) {
+	    	return Object.assign({}, state, {
+    			feedback: 'You\'re Hot!',
+    			guesses: [...state.guesses, action.guess]
+    		});
+	    } else {
+	    	return Object.assign({}, state, {
+    			feedback: 'You got it!',
+    			guesses: [...state.guesses, action.guess]
+    		});
+	    }
 	} //actions.MAKE_GUESS
 
 	if (action.type === actions.RESTART_GAME) {
@@ -20,11 +52,24 @@ export const reduxHotCold = (state=initialState, action) => {
 		});
 	} //actions.RESTART_GAME
 
-	if (action.type === actions.AURAL_UPDATE) {
-		return Object.assign({}, state, {
-			//auralStatus
-		});
+	else if (action.type === actions.AURAL_UPDATE) {
+		const pluralize = state.guesses.length !== 1;
+		let  auralStatus = `Here's the status of the game right now: ${state.feedback} You've made ${state.guesses.length} ${pluralize ? 'guesses' : 'guess'}.`;
+		document.title = state.feedback ? `${state.feedback} | Hot or Cold` : 'Hot or Cold';
+		if (state.guesses.length > 0) {
+      		auralStatus += ` ${pluralize ? 'In order of most- to least-recent, they are' : 'It was'}: ${state.guesses.reverse().join(', ')}`;
+    		return Object.assign({}, state, {
+				auralStatus
+			});
+    	} //if (guesses.length > 0)
+    	else {
+    		return Object.assign({}, state, {
+				auralStatus
+			}); 
+    	} //else
 	} //actions AURAL_UPDATE
+
+
 
 	return state;
 	
