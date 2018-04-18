599a168ffe985cf58fd05e29a5e5254bae597b14
Benilyn <benilyn.gonzales@gmail.com>
2018-04-18 11:08:57 -0700

    update code to use redux state management

--- src/components/top-nav.js
+++ src/components/top-nav.js
@@ -1,42 +1,50 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
+import {restartGame, auralUpdate} from '../actions';
 
 import './top-nav.css';
 
-export default function TopNav(props) {
-  return (
-    <nav>
-      <ul className="clearfix">
-        <li>
-          <a 
-            href="#what" 
-            className="what"
-            aria-label="How to play"
-          >
-            What?
-          </a>
-        </li>
-        <li>
-          <a
-            href="#feedback"
-            className="new"
-            aria-label="Start a new game"
-            onClick={() => props.onRestartGame()}
-          >
-            + New Game
-          </a>
-        </li>
-        <li>
-          <a
-            href="#get-status"
-            /* the `visuallyhidden` class hides an element 
-            while leaving it available to screen reader users  */
-            className="visuallyhidden focusable status-link"
-            onClick={() => props.onGenerateAuralUpdate()}
-          >
-            Hear state of game
-          </a>
-        </li>
-      </ul>
-    </nav>
-  );
-}
+export class TopNav extends React.Component {
+  restartGame() {
+    this.props.dispatch(restartGame());
+  } //restartGame()
+
+  auralUpdate() {
+    this.props.dispatch(auralUpdate());
+  } //auralUpdate()
+  
+  render() {
+    return (
+      <div>
+          <ul className="clearfix">
+            <li><a 
+                href="#what" 
+                className="what" 
+                aria-label="How to play">
+                What?</a>
+            </li> {/*href="#what"*/}
+            <li><a 
+                href="#feedback" 
+                className="new" 
+                aria-label="Start a new game"
+                onClick={() => this.onRestartGame()}>
+                + New Game</a>
+            </li> {/*href="#feedback"*/}
+            <li><a                         
+                href="#get-status" 
+                className="visuallyhidden focusable status-link"
+                onClick={() => this.onGenerateAuralUpdate()}>
+                Hear state of game</a>
+            </li> {/*href="#get-status"*/}
+          </ul>
+      </div>
+    ); 
+  } 
+  
+    
+} //export class TopNav
+
+export default connect()(TopNav);
+
+  
